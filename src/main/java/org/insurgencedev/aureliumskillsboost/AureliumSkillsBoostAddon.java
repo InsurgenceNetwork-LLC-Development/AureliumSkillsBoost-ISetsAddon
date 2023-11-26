7567f96d0869f4f48b398dc3906e5d3b75d01f4c
iHxtch <84823076+iHxtch@users.noreply.github.com>
2023-11-26 09:43:48 -0500

    init

---
+++ src/main/java/org/insurgencedev/aureliumskillsboost/AureliumSkillsBoostAddon.java
@@ -0,0 +1,20 @@
+package org.insurgencedev.aureliumskillsboost;
+
+import org.bukkit.Bukkit;
+import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
+import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
+
+@ISetsAddon(name = "AureliumSkillsBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "Boost the skill experience earned from AureliumSkills")
+public class AureliumSkillsBoostAddon extends InsurgenceSetsAddon {
+
+    @Override
+    public void onAddonStart() {
+        if (isDependentEnabled()) {
+            registerEvent(new SkillsXPGainListener());
+        }
+    }
+
+    private boolean isDependentEnabled() {
+        return Bukkit.getPluginManager().isPluginEnabled("AureliumSkills");
+    }
+}
